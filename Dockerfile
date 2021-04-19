f542c55c50bfe4d3dab2696f7b20f8e52169ce99
Anupam Dahal <miracleanupam@gmail.com>
2021-04-20 01:56:50 +0300

    moved runserver command to entrypoint and put only entrypoint as cmd in compose

--- Dockerfile
+++ Dockerfile
@@ -18,7 +18,7 @@ RUN mv djangodemo/* .
 RUN pip install -r requirements.txt
 
 RUN chmod +x entrypoint.sh
-RUN ./entrypoint.sh
+#RUN ./entrypoint.sh
 
 EXPOSE 8000
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,5 @@ sleep 10
 python manage.py migrate
 python manage.py createsuperuser --noinput
 python manage.py init_users
+
+python manage.py runserver 0.0.0.0:8000
